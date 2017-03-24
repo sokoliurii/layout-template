d487176f599b5d536a830e849a831b69b7103fbf
sokoliurii <sokoliurii@gmail.com>
2017-03-24 14:54:33 +0300

    font-face update

--- tasks/assets/font-face.js
+++ tasks/assets/font-face.js
@@ -1,15 +1,12 @@
 import fs from 'fs';
 import path from 'path';
 
-
-const fontsDir = 'app/fonts/';
-const fontsFile = 'app/sass/base/_typography.scss';
-const fontsCSSDir = '../fonts/';
+const fontsDir = 'app/fonts/'; // Директория шрифтовых файлов
+const fontsCSSDir = '../fonts/'; // Путь относительно к папке со шрифтами относительно файла типографики
+const fontsFile = 'app/sass/base/_typography.scss'; // Путь к файлу типографики
 
 let fontsPaths = [];
 let fontsNames = [];
-let fontName = '';
-let fontExtension = '';
 let fontFaces = [];
 
 
@@ -19,108 +16,119 @@ fontsPaths = fs.readdirSync(fontsDir);
 
 
 
-/** Если среди шрифтов есть FontAwesome, убираем его из массива **/
-let index = fontsPaths.indexOf('font-awesome');
-if(index != -1) {
-	fontsPaths.splice(index, 1);
-}
-
-
-
 /** Получаем список шрифтов без расширений **/
-fontsPaths.map((font) => {
-	fontName = path.parse(font).name;	
+fontsPaths.map(font => {
+	let fontName = path.parse(font).name;
 	if(!fontsNames.includes(fontName)) {
-		fontsNames.push(fontName);
+		fontsNames.push(path.parse(font).name);
 	}
 });
 
 
 
 /** Генерируем массив с объектами семейств шрифтов **/
-fontsNames.map((fontName) => {
+fontsNames.map(fontName => {
 	let font = {};
 	let fontFamily = '';
-	
-	let fontRegExp = new RegExp(/ultra-light|ultralight|thin|light|light-italic|lightitalic|regular|italic|medium|semibold|semibold-italic|semibolditalic|bold|bold-italic|bolditalic|extrabold|extrabold-italic|extrabolditalic|black|heavy/i);
+	let fontExtension = '';
+	let fontRegExp = new RegExp(/ultra|light|thin|italic|regular|medium|semi|bold|extra|black|heavy/gi);
+
+	/** Имя гарнитуры **/
 	fontFamily = fontName.replace(fontRegExp, '').replace('-', '');
 	fontFamily = fontFamily.charAt(0).toUpperCase() + fontFamily.slice(1).trim();
-
 	font.name = fontFamily;
 
 	font.src = '';
-	fontsPaths.map((fontPath) => {
-		if(fontName == path.parse(fontPath).name) {
-			fontExtension = path.extname(fontPath).replace('.', '');
-			if(font.src == '') {
-				font.src = `url("${fontsCSSDir + fontPath}") format("${fontExtension}")`;
-			}
-			else {
-				font.src += `, url("${fontsCSSDir + fontPath}") format("${fontExtension}")`;
-			}
-		}
+	fontsPaths.map(fontPath => {
+
+	/** Относительный путь на шрифтовой файл **/
+	if(fontName == path.parse(fontPath).name) {
+		fontExtension = path.extname(fontPath).replace('.', '');
+		let url = `url("${fontsCSSDir + fontPath}") format("${fontExtension}")`;
+		font.src == '' ? font.src = url : font.src += ',\n\t\t' + url;
+	}
 	});
 
-
-	if(fontName.search(/ultra-light|ultralight/i) > 0) {
+	/**
+	 * Приводим имя файла к единому виду и передаем
+	 * значения font-weight и font-style гарнитуры,
+	 * а также начертание гарнитуры для вывода в консоль
+	**/
+	fontName = fontName.toLowerCase().replace('-', '');
+	if(fontName.search(/light/i) > 0 && fontName.search(/ultralight/i) < 0 && fontName.search(/lightitalic/i) < 0) {
+		font.weight = 300;
+		font.style = 'normal';
+		font.styleName = 'Light';
+	}
+	if(fontName.search(/italic/i) > 0 && fontName.search(/lightitalic/i) < 0 && fontName.search(/semibolditalic/i) < 0 && fontName.search(/bolditalic/i) < 0 && fontName.search(/extrabolditalic/i) < 0) {
+		font.weight = 400;
+		font.style = 'italic';
+		font.styleName = 'Italic';
+	}
+	if(fontName.search(/bold/i) > 0 && fontName.search(/semibold/i) < 0 && fontName.search(/semibolditalic/i) < 0 && fontName.search(/bolditalic/i) < 0 && fontName.search(/extrabolditalic/i) < 0) {
+		font.weight = 700;
+		font.style = 'normal';
+		font.styleName = 'Bold';
+	}
+	if(fontName.search(/semibold/i) > 0 && fontName.search(/semibolditalic/i) < 0) {
+		font.weight = 600;
+		font.style = 'normal';
+		font.styleName = 'Semibold';
+	}
+	if(fontName.search(/ultralight/i) > 0) {
 		font.weight = 100;
     font.style = 'normal';
+		font.styleName = 'Ultra Light';
 	}
 	if(fontName.search(/thin/i) > 0) {
 		font.weight = 200;
     font.style = 'normal';
+		font.styleName = 'Thin';
 	}
-	if(fontName.search(/light/i) > 0) {
-		font.weight = 300;
-    font.style = 'normal';
-	}
-	if(fontName.search(/light-italic|lightitalic/i) > 0) {
+	if(fontName.search(/lightitalic/i) > 0) {
 		font.weight = 300;
     font.style = 'italic';
+		font.styleName = 'Light Italic';
 	}
 	if(fontName.search(/regular/i) > 0) {
 		font.weight = 400;
     font.style = 'normal';
-	}
-	if(fontName.search(/italic/i) > 0) {
-		font.weight = 400;
-    font.style = 'italic';
+		font.styleName = 'Regular';
 	}
 	if(fontName.search(/medium/i) > 0) {
 		font.weight = 500;
     font.style = 'normal';
+		font.styleName = 'Medium';
 	}
-	if(fontName.search(/semibold/i) > 0) {
-		font.weight = 600;
-    font.style = 'normal';
-	}
-	if(fontName.search(/semibold-italic|semibolditalic/i) > 0) {
+	if(fontName.search(/semibolditalic/i) > 0) {
 		font.weight = 600;
     font.style = 'italic';
+		font.styleName = 'Semibold Italic';
 	}
-	if(fontName.search(/bold/i) > 0) {
-		font.weight = 700;
-		font.style = 'normal';
-	}
-	if(fontName.search(/bold-italic|bolditalic/i) > 0) {
+	if(fontName.search(/bolditalic/i) > 0 && fontName.search(/semibolditalic/i) < 0) {
 		font.weight = 700;
     font.style = 'italic';
+		font.styleName = 'Bold Italic';
 	}
 	if(fontName.search(/extrabold/i) > 0) {
 		font.weight = 800;
 		font.style = 'normal';
+		font.styleName = 'Extrabold';
 	}
-	if(fontName.search(/extrabold-italic|extrabolditalic/i) > 0) {
+	if(fontName.search(/extrabolditalic/i) > 0) {
 		font.weight = 800;
     font.style = 'italic';
+		font.styleName = 'Extrabold Italic';
 	}
 	if(fontName.search(/black|heavy/i) > 0) {
 		font.weight = 900;
     font.style = 'normal';
+		font.styleName = 'Black';
 	}
 	if(fontName.search(fontRegExp) == -1) {
 		font.weight = 400;
     font.style = 'normal';
+		font.styleName = 'Regular';
 	}
 
 	fontFaces.push(font);
@@ -133,8 +141,8 @@ fs.writeFile(fontsFile, '');
 
 
 
-/** Подставляем код в файл типографики **/
-fontFaces.map((font) => {
+/** Подставляем код в файл типографики и делаем вывод в консоль **/
+fontFaces.map(font => {
 	let fontFace = `@font-face {
   font-family: "${font.name}";
   src: ${font.src};
@@ -146,6 +154,6 @@ fontFaces.map((font) => {
 
 	fs.appendFile(fontsFile, fontFace, function(err)  {
 		if(err) return err;
-	})
-	console.log(`Добавлен шрифт: ${font.name}`);
+		console.log(`Добавлен шрифт ${font.name}, с начертанием ${font.styleName}.`);
+	});
 });
